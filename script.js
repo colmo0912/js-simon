c19b13519da509105ec37a74bcb77ab1766243f8
colmo0912 <colmo0912@gmail.com>
2021-04-21 17:43:02 +0200

    da rivedere confronto array

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-// creo delle variabili e scrivo un ciclo while per generare 5 numeri casuali e li inserisco dentro un array.
-
+// creo delle variabili e scrivo un ciclo while per generare 5 numeri casuali e 
+//li inserisco dentro un array.
 
 var numeriRandom = [];
 var numeriUtente = [];
@@ -10,7 +10,6 @@ while (numeriRandom.length < 5) {
     var numero = Math.floor(Math.random() * 50) + 1;
 
     if(numeriRandom.indexOf(numero) === -1){
-
         numeriRandom.push(numero)
     }
 }
@@ -19,20 +18,35 @@ while (numeriRandom.length < 5) {
 
 alert("Ciao! i 5 numeri generati dal PC sono: " + numeriRandom + " Hai 30 secondi per memorizzarli e riscriverli!");
 
-//inseriso un timer di 30 secondi e inserisco un for con i 5 prompt dove inserire i numeri dell'utente
+//inseriso un timer di 30 secondi e inserisco un for con i 5 prompt dove 
+//inserire i numeri dell'utente
 setTimeout (inputNumeriUtente, 30000);
 
 function inputNumeriUtente(){
 
     for(i=0 ; i<5; i++){
-
-        var imputUtente = parseInt(prompt("Scrivi in successione i numeri che hai letto nella schermata precendente"));
-
-        // inserisco i numeri inseriti dall'utente dentro l'array numeriUtente
+        var imputUtente = parseInt(prompt("Scrivi i 5 numeri che hai letto nella schermata precendente"));
         numeriUtente.push(imputUtente);
-        console.log(numeriUtente)
-
     }
+     // inserisco i numeri inseriti dall'utente dentro l'array numeriUtente
+    console.log(numeriUtente)
+
+}
+
+//confronto tra gli array
+
+var numeroTrovato = false
+
+for(var j=0; j < numeriRandom.length; j++)
+
+var numeroRandomComparato = numeriRandom[j]
+
+if(numeroRandomComparato == numeriUtente[j]){
+    numeroTrovato = true;
+    console.log("il numero trovato è " + numeroTrovato)
+} if (!numeroTrovato) {
+
+    console.log("Non hai trovato numeri")
 }
 
 
